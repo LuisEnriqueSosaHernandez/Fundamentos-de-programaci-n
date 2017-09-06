ac7c116688ee29db5037450eb7463ecbc1306e6e
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2017-09-05 22:23:37 -0500

    Intercambio

--- Codigos/Intercambio/main.cpp
+++ Codigos/Intercambio/main.cpp
@@ -5,17 +5,30 @@ using namespace std;
 
 int main()
 {
-    int a[]={0,1,2,3,4,5,6,7,8,9};
-    int puntero1=0,puntero2=5,aux;
-    do{
-        aux=a[puntero2];
+    int a[10];//arreglo
+    int puntero1,puntero2,aux;//variables de punteros y aux para el metodo burbuja
+    char resp;//variable para la respuesta del usuario si quiere repetir o no
+    do{//ciclo do while para repetir el proceso completo
+        puntero1=0;//puntero que reccorre la primera mitad del arreglo
+    puntero2=5;//puntero que recorre la segunda mitad del arreglo
+             for(int i=0;i<10;i++){//ciclo para ingresar datos en el arreglo
+                    cout<<"Ingrese el valor: "<<i<<endl;
+                    cin>>a[i];
+        }
+       while(puntero1<5&&puntero2<10){//ciclo que permite ordenar de la forma deseada los datos auspiciado por el metodo burbuja
+         aux=a[puntero2];
         a[puntero2]=a[puntero1];
         a[puntero1]=aux;
         puntero1++;
         puntero2++;
-    }while(puntero1<5&&puntero2<10);
-    for(int i=0;i<10;i++){
+       }
+       cout<<"Ordenados"<<endl;
+        for(int i=0;i<10;i++){//Impresion de los datos ordenados o mas bien desordenados xD
 cout<<a[i]<<endl;
         }
+        cout<<"Repetir s=si n=no"<<endl;
+        cin>>resp;//entrada de respuesta del usuario a peticion de repeticion
+    }while(resp!='n');//fin del ciclo do while cuando el usuario ingrese n
+
     return 0;
 }
